9912f3ac0a65189d688adc373dba9b504329f75f
gal <galhyt@gmail.com>
2024-05-19 19:35:00 +0300

    count_assignments.py - first version

--- autobrains/count_assignments.py
+++ autobrains/count_assignments.py
@@ -1,3 +1,5 @@
+import functools
+import itertools
 from typing import List, Tuple, Optional, Dict, Callable, Union, Any
 
 from hint import Hint, AbsoluteHint, NeighbourHint, RelativeHint
@@ -5,97 +7,44 @@ from hint import Hint, AbsoluteHint, NeighbourHint, RelativeHint
 
 def count_assignments(hints: List[Hint]):
     floors: List[List[Union[Hint.Color, Hint.Animal, None]]] = [[None, None] for _ in range(5)]
-
-    def _search_animal_floor(animal: Hint.Animal):
-        try:
-            f = next(filter(lambda i: floors[i][1] == animal, range(len(floors))))
-            return f
-        except StopIteration:
-            return None
-
-    def _search_color_floor(color: Hint.Color):
-        try:
-            f = next(filter(lambda i: floors[i][0] == color, range(len(floors))))
-            return f
-        except StopIteration:
-            return None
+    cand_colors = list(Hint.Color)
+    cand_animals = list(Hint.Animal)
 
     def _get_animal_free_floors():
-        return set(filter(lambda i: floors[i][1] is None, range(len(floors))))
+        return list(filter(lambda i: floors[i][1] is None, range(len(floors))))
 
     def _get_color_free_floors():
-        return set(filter(lambda i: floors[i][0] is None, range(len(floors))))
+        return list(filter(lambda i: floors[i][0] is None, range(len(floors))))
 
     def _set_absolute_floor_hint(hint: AbsoluteHint):
         a, b = hint.value
-        if isinstance(a, Hint.Floor):
-            if isinstance(b, Hint.Color):
-                floors[a.value-1][0] = b
-            else:
-                floors[a.value-1][1] = b
-
-    def _set_absolute_animal_hint(hint: AbsoluteHint):
-        a, c = hint.value
-        a_f = _search_animal_floor(a)
-        c_f = _search_color_floor(c)
-        if a_f is not None:
-            floors[a_f][0] = c
-        elif c_f is not None:
-            floors[c_f][1] = a
+        if isinstance(b, Hint.Color):
+            floors[a.value-1][0] = b
+            cand_colors.remove(b)
         else:
-            animal_free = _get_animal_free_floors()
-            color_free = _get_color_free_floors()
-            candidates = animal_free.intersection(color_free)
-            if len(candidates) == 1:
-                i = candidates.pop()
-                floors[i] = [c, a]
-
-    def _set_neighbour_hint(hint: NeighbourHint):
-        def _get_b_f(a, b, a_f, b_f):
-            if a_f is None and b_f is None:
-                return
-            if a_f is None:
-                a_f, b_f = b_f, a_f
-                a, b = b, a
-            if a_f == 0:
-                b_f = 1
-            elif a_f == 4:
-                b_f = 3
-            elif floors[a_f-1][1]:
-                b_f = a_f + 1
-            elif floors[a_f+1][1]:
-                b_f = a_f - 1
-
-            return b, b_f
-
-        a, b = hint.value
-        if isinstance(a, Hint.Animal):
-            a_f, b_f = _search_animal_floor(a), _search_animal_floor(b)
-        else:
-            a_f, b_f = _search_color_floor(a), _search_color_floor(b)
-
-        b, b_f = _get_b_f(a, b, a_f, b_f)
-        if b_f is not None:
-            floors[b_f][1] = b
-
-    def _set_relative_hint(hint: RelativeHint):
-        a, b, n = hint.value
-        a_f = _search_animal_floor(a) if isinstance(a, Hint.Animal) else _search_color_floor(a)
-        b_f = _search_animal_floor(b) if isinstance(a, Hint.Animal) else _search_color_floor(b)
-
-
+            floors[a.value-1][1] = b
+            cand_animals.remove(b)
 
     for hint in filter(lambda h: isinstance(h, AbsoluteHint) and isinstance(h.value[0], Hint.Floor), hints):
         _set_absolute_floor_hint(hint)
 
-    for hint in filter(lambda h: isinstance(h, AbsoluteHint) and isinstance(h.value[0], Hint.Animal), hints):
-        _set_absolute_animal_hint(hint)
+    valid_assignments = 0
 
-    for hint in filter(lambda h: isinstance(h, NeighbourHint), hints):
-        _set_neighbour_hint(hint)
+    color_free = _get_color_free_floors()
+    animal_free = _get_animal_free_floors()
 
-    for hint in filter(lambda h: isinstance(h, RelativeHint), hints):
-        _set_relative_hint(hint)
+    for colors_comb in itertools.permutations(cand_colors, len(cand_colors)):
+        for i, f in enumerate(color_free):
+            floors[f][0] = colors_comb[i]
+        for animal_comb in itertools.permutations(cand_animals, len(cand_animals)):
+            for j, f in enumerate(animal_free):
+                floors[f][1] = animal_comb[j]
+            # check if assignment valid
+            if all((h.is_assignment_valid(floors) for h in hints)):
+                valid_assignments += 1
+
+    return valid_assignments
+
 
 if __name__ == '__main__':
     hints = [AbsoluteHint((Hint.Floor.first, Hint.Animal.rabbit)),
@@ -104,5 +53,6 @@ if __name__ == '__main__':
              AbsoluteHint((Hint.Floor.fifth, Hint.Animal.bird)),
              AbsoluteHint((Hint.Animal.grasshopper, Hint.Color.blue)),
              NeighbourHint((Hint.Color.red, Hint.Color.green))]
-
-    count_assignments(hints)
+    c = count_assignments(hints)
+    print(c)
+    assert c == 2
--- autobrains/hint.py
+++ autobrains/hint.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum, auto
-from typing import Dict, Tuple, Union
+from typing import Dict, Tuple, Union, List, Optional
 
 
 @dataclass
@@ -28,18 +28,60 @@ class Hint:
         yellow = auto()
         orange = auto()
 
+    def is_assignment_valid(self, floors: List[List[Union[Color, Animal]]]) -> bool:
+        raise NotImplemented(f"is_assignment_valid not implemented in {self.__class__}")
+
+    def search_animal_floor(self, floors: List[List[Union[Color, Animal]]], animal: Animal) -> Optional[int]:
+        try:
+            f = next(filter(lambda i: floors[i][1] == animal, range(len(floors))))
+            return f
+        except StopIteration:
+            return None
+
+    def search_color_floor(self, floors: List[List[Union[Color, Animal]]], color: Color) -> Optional[int]:
+        try:
+            f = next(filter(lambda i: floors[i][0] == color, range(len(floors))))
+            return f
+        except StopIteration:
+            return None
+
 
 class AbsoluteHint(Hint):
 
     def __init__(self, value: Tuple[Union[Hint.Floor, Hint.Animal], Union[Hint.Color, Hint.Animal]]):
         self.value = value
 
+    def is_assignment_valid(self, floors: List[List[Union[Hint.Color, Hint.Animal]]]) -> bool:
+        a, b = self.value
+        if isinstance(a, Hint.Floor):
+            color, animal = floors[a.value-1]
+            return color == b if isinstance(b, Hint.Color) else animal == b
+        else:
+            a_f = self.search_animal_floor(floors, a)
+            color, animal = floors[a_f]
+            return b == color
+
 
 class RelativeHint(Hint):
     def __init__(self, value: Tuple[Union[Hint.Animal, Hint.Color], Union[Hint.Animal, Hint.Color], int]):
+        # value - how many floors the first element in the tuple resides below the second one
         self.value = value
 
+    def is_assignment_valid(self, floors: List[List[Union[Hint.Color, Hint.Animal]]]) -> bool:
+        a, b, n = self.value
+        a_f = self.search_animal_floor(floors, a) if isinstance(a, Hint.Animal) else self.search_color_floor(floors, a)
+        b_f = self.search_animal_floor(floors, b) if isinstance(b, Hint.Animal) else self.search_color_floor(floors, b)
+
+        return b_f - a_f == n
 
 class NeighbourHint(Hint):
     def __init__(self, value: Tuple[Union[Hint.Animal, Hint.Color], Union[Hint.Animal, Hint.Color]]):
         self.value = value
+
+    def is_assignment_valid(self, floors: List[List[Union[Hint.Color, Hint.Animal]]]) -> bool:
+        a, b = self.value
+        a_f = self.search_animal_floor(floors, a) if isinstance(a, Hint.Animal) else self.search_color_floor(floors, a)
+        b_f = self.search_animal_floor(floors, b) if isinstance(b, Hint.Animal) else self.search_color_floor(floors, b)
+
+        arr = sorted([a_f, b_f])
+        return arr[1] == arr[0]+1

0f27213493caa7fc2e814c1cfbcb9d7d853aca0f
gal <galhyt@gmail.com>
2024-05-24 22:42:30 +0300

    umbrellas.py - fix bug

--- umbrellas.py
+++ umbrellas.py
@@ -1,15 +1,20 @@
-import itertools
+from functools import reduce
 from typing import List
 
 
 def min_set(umbrellas: List[int], people: int):
     umbrellas = sorted(umbrellas, reverse=True)
+    min_comb = {}
+    min_comb_umb = 2 ** 31
     for j in range(len(umbrellas)):
         u = umbrellas[j]
         n = people // u
         comb = {u: n}
         if people % u == 0:
-            return comb
+            umb_num = reduce(lambda s, u: s + comb[u], comb, 0)
+            if umb_num < min_comb_umb:
+                min_comb = comb.copy()
+                min_comb_umb = umb_num
 
         if j < len(umbrellas) - 1:
             for i in range(n, 0, -1):
@@ -17,13 +22,16 @@ def min_set(umbrellas: List[int], people: int):
                 _comb = min_set(umbrellas[j+1:], people - u*i)
                 if _comb:
                     comb.update(_comb)
-                    return comb
-
-    return []
+                    umb_num = reduce(lambda s, u: s+comb[u] , comb, 0)
+                    if umb_num < min_comb_umb:
+                        min_comb = comb.copy()
+                        min_comb_umb = umb_num
+    return min_comb
 
 
 if __name__ == '__main__':
-    umbrellas = [3, 6, 7, 8, 19, 21, 31]
-    people = 51
-    # 7: 3, 6: 4, 3: 8
+    umbrellas = [186,419,83,408]
+    people = 6249
+    # umbrellas = [2, 3, 6, 7, 8]
+    # people = 66
     print(min_set(umbrellas, people))

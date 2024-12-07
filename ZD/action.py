5134aa63e88e912c37509e8972bb0e637860a636
gal <galhyt@gmail.com>
2024-12-07 21:18:16 +0200

    ZD Actions - check transition validity

--- ZD/action.py
+++ ZD/action.py
@@ -8,6 +8,7 @@ from typing import List, Callable
 class ZDState:
     name: str
     initial: bool = False
+    transitions = []
 
     def to(self, target):
         return Transition(self, target)
@@ -21,19 +22,21 @@ class Transition:
     execute_callback: Callable = None
     execute_callback_succeeded: Callable = None
 
-    def execute(self):
-        succeeded = self.execute_callback()
+    def __call__(self, *args, **kwargs):
+        succeeded = self.execute_callback(*args, **kwargs) if self.execute_callback else True
         if succeeded:
             self.execute_callback_succeeded()
+
+    def __str__(self):
+        return f"Transition {self.name}: {self.src.name} -> {self.trg.name}"
 
 
 class ZDStateMachineMetaClass(ABCMeta):
 
     def __new__(mcs, *args, **kwargs):
         cls = super().__new__(mcs, *args, **kwargs)
-        parent, = args[1]
 
-        if parent is not ABC:
+        if all((parent is not ABC for parent in args[1])):
             states = []
             transitions = []
 
@@ -48,6 +51,9 @@ class ZDStateMachineMetaClass(ABCMeta):
             setattr(cls, "states", states)
             setattr(cls, "transitions", transitions)
 
+            for transition in cls.transitions:
+                transition.src.transitions.append(transition)
+
         return cls
 
     def __call__(cls, *args, **kwargs):
@@ -56,19 +62,25 @@ class ZDStateMachineMetaClass(ABCMeta):
         for transition in self.transitions:
             fn_name = f"on_{transition.name}"
             fn = getattr(cls, fn_name, None)
-            if fn:
-                transition.execute_callback = partial(fn, self)
-                transition.execute_callback = partial(cls._after_transition_, self, transition.trg)
+            transition.execute_callback = partial(cls._on_transition_, fn, self, transition)
+            transition.execute_callback_succeeded = partial(cls._after_transition_, self, transition.trg)
 
         init_state = [state for state in self.states if state.initial]
         if init_state:
-            self.cur_state = init_state
+            self.cur_state = init_state[0]
 
         return self
 
     @staticmethod
     def _after_transition_(state_machine, trg_state):
         state_machine.cur_state = trg_state
+
+    @staticmethod
+    def _on_transition_(fn, state_machine, transition):
+        print(f"{transition}")
+        if state_machine.cur_state != transition.src:
+            raise TypeError(f"Transition {transition.name} can't be executed from state {state_machine.cur_state.name}")
+        return fn(state_machine) if fn else True
 
 
 class ZDStateMachine(ABC, metaclass=ZDStateMachineMetaClass):
@@ -83,7 +95,10 @@ class ZDStateMachine(ABC, metaclass=ZDStateMachineMetaClass):
 class Action(ZDStateMachine):
 
     def move_next(self):
-        pass
+        for tran in self.cur_state.transitions:
+            if tran():
+                return True
+        return False
 
 
 class AddAction(Action):
@@ -97,12 +112,14 @@ class AddAction(Action):
     finish = machine_running.to(final)
 
     def on_start(self):
-        print("om_start")
-        return True
+        print("on_start")
+        return False
 
 
 if __name__ == '__main__':
     add = AddAction()
     print(f"initial state = {add.cur_state}")
-    add.start.execute()
+    add.start()
+    print(f"initial state = {add.cur_state}")
+    add.start_machine()
     print(f"initial state = {add.cur_state}")
